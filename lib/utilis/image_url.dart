b0d96955868dc4154d41c75306b5369054a909b2
MD shakil Hossain <mdshakilhossain720@gmail.com>
2025-05-07 22:26:12 +0600

    image add

---
+++ lib/utilis/image_url.dart
@@ -0,0 +1,13 @@
+class ImageUrl {
+  static String imageUrl = "assets/images/";
+
+  static const logo = "assets/images/appbar.png";
+  // auth
+  static const login = "assets/images/login.png";
+  static const apple = "assets/images/apple_con.png";
+
+  static const facebook = "assets/images/facebook.png";
+  static const google = "assets/images/googlecon.png";
+  static const lock = "assets/images/lock.png";
+  static const otp = "assets/images/opt.png";
+}
